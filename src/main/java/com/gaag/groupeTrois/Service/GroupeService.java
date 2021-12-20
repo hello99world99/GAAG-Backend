98af7b11b20837c7c171d61506a24f6bd7669800
Abdoulm <abdelshmurda@gmail.com>
2021-12-20 20:26:29 +0000

    commit de la Groupe

---
+++ src/main/java/com/gaag/groupeTrois/Service/GroupeService.java
@@ -0,0 +1,11 @@
+package com.gaag.groupeTrois.Service;
+
+import com.gaag.groupeTrois.Model.Groupe;
+
+import java.util.List;
+
+public interface GroupeService {
+
+
+    List<Groupe> afficherAllGroupe();
+}
